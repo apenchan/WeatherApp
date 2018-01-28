7465403009262656fa4cd7f263d9319d0c8542c8
anna Penchansky <annapenchansky@annas-mbp.mynet>
2018-01-28 14:10:39 +0200

    had made this a while ago using React, but repo was set to private. using all my original code, but I am using a webpack config I had initially cloned. making everything public now

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Header from './common/Header';
+import Routes from './Routes';
+import {BrowserRouter} from 'react-router-dom';
+
+class AppWrapper extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Header/>
+          <div className="container">
+            <Routes/>
+          </div>
+        </div>
+      </BrowserRouter>
+
+    );
+  }
+}
+
+ReactDom.render(
+  <AppWrapper/>, document.getElementById('react-app'));
