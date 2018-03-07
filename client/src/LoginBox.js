7427e0406e08dde9de85be00b45118496e68ef35
anna Penchansky <annapenchansky@annas-MacBook-Pro.local>
2018-03-07 15:43:09 -0500

    doing the login form. programming running slow, so will close and then restart

---
+++ client/src/LoginBox.js
@@ -0,0 +1,13 @@
+//May not need, or just to put the form inside this component
+
+// import React, { Component } from 'react';
+
+// class LoginBox extends Component{
+//   render(){
+//     return(
+//       <div className="login-username">
+//         {this.props.username}
+//       </div>
+//     )
+//   }
+// }
